f1e8154455100d8d35e473c68f610d592898c906
saurabh <saurabh@gmail.com>
2021-12-23 13:08:21 +0530

    Hooks in React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+import Style from './Style.css';
+
+
+// useState is basically a function which return a two items as an array
+// 2 data 1st is current data and the 2nd one is updated data.
+
+
+
 
 function App() {
+
+  const state = useState();
+
+  const [count, setCount] = useState(0);
+
+
+  // let count = 0;
+
+
+  const IncMen = () => {
+    setCount(count + 1);
+    // console.log("Clicked "+ count++);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+        <div className='item'>
+          <h1 className='count'> {count}</h1>
+          <button className='btn' onClick={IncMen}>  Click me </button>
+        </div>
+
+      </div>
+
+
+    </>
+
+
   );
 }
 
