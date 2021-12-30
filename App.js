8f16dcf51634f5f0692d9ca432f9f8e62d9c1a68
Saurabh Kumar Singh <70137970+saurabh-singh-au13@users.noreply.github.com>
2021-12-30 12:25:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import Hooks from './components/Hook/Hooks'
+import Forms from './components/Form/Forms'
+
+
+function App() {
+  return (
+    <div>
+      {/* <Hooks /> */}
+      <Forms />
+      
+    </div>
+  )
+}
+
+export default App
